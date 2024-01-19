7aee957759e57719084dfdeb762bfe140772df56
Ali İbrahim Temiz <ibrahimtmz2001@gmail.com>
2024-01-19 17:51:02 +0300

    Project Files

---
+++ Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Objects/Dungeon/Room Node Graph")]
+public class RoomNodeGraphSO : ScriptableObject
+{
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+    [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
+    [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
+
+    private void Awake()
+    {
+        LoadRoomNodeDictionary();
+    }
+
+    /// <summary>
+    /// Load the room node dictionary from the room node list.
+    /// </summary>
+    private void LoadRoomNodeDictionary()
+    {
+        roomNodeDictionary.Clear();
+
+        // Populate dictionary
+        foreach (RoomNodeSO node in roomNodeList)
+        {
+            roomNodeDictionary[node.id] = node;
+        }
+    }
+
+    /// <summary>
+    /// Get room node by room nodeID
+    /// </summary>
+    public RoomNodeSO GetRoomNode(string roomNodeID)
+    {
+        if (roomNodeDictionary.TryGetValue(roomNodeID, out RoomNodeSO roomNode))
+        {
+            return roomNode;
+        }
+        return null;
+    }
+
+    #region Editor Code
+
+    // The following code should only run in the Unity Editor
+#if UNITY_EDITOR
+
+    [HideInInspector] public RoomNodeSO roomNodeToDrawLineFrom = null;
+    [HideInInspector] public Vector2 linePosition;
+
+    // Repopulate node dictionary every time a change is made in the editor
+    public void OnValidate()
+    {
+        LoadRoomNodeDictionary();
+    }
+
+    public void SetNodeToDrawConnectionLineFrom(RoomNodeSO node, Vector2 position)
+    {
+        roomNodeToDrawLineFrom = node;
+        linePosition = position;
+    }
+
+#endif
+
+    #endregion Editor Code
+}
